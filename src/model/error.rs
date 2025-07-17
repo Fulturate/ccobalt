1b7db6afaac3c925aeb2a64430e5e1b077169ec1
iku <121695593+notiku@users.noreply.github.com>
2025-07-17 16:04:50 +0200

    error: Check YouTube service disabled check

--- src/model/error.rs
+++ src/model/error.rs
@@ -47,6 +47,7 @@ impl fmt::Display for CobaltError {
             "error.api.youtube.decipher" => f.write_str("Cannot decipher that video. Something probably broke."),
             "error.api.youtube.login" => f.write_str("That video requires a logged in account, which we do not have."),
             "error.api.youtube.token_expired" => f.write_str("Our YouTube token expired (try again later)"),
+            "error.api.youtube.temporary_disabled" => f.write_str("YouTube support is temporarily disabled. Try again later."),
             _ => f.write_str(&self.code),
         }
     }
